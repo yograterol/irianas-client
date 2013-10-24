7c83f857754a2439e3039680d29cfcb705d0c3d1
Yohan Graterol <yohangraterol92@gmail.com>
2013-10-24 14:22:23 -0430

    Add decorator for require token

---
+++ irianas_client/decorators.py
@@ -0,0 +1,25 @@
+import os
+import hashlib
+import simplejson as json
+from flask import request
+from flaks_restful import abort
+from irianas_clent.api import path_file_token
+
+
+def require_token(f):
+    def inner(*args, **kwargs):
+        if os.path.exists(path_file_token):
+            file_token = open(path_file_token)
+            data_json = json.loads(file_token)
+
+            token = hashlib.sha512(request.form.data['token']).hexdigest()
+            ip = hashlib.sha512(request.form.data['ip']).hexdigest()
+
+            if data_json.get('token') == token and \
+               data_json.get('ip') == ip:
+                return f(*args, **kwargs)
+            else:
+                return abort(401)
+        else:
+            return abort(401)
+    return inner
