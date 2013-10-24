34372dfcf42daae10277852ff6553dc553080632
Yohan Graterol <yohangraterol92@gmail.com>
2013-10-24 15:12:16 -0430

    Fix decorator

--- irianas_client/decorators/__init__.py
+++ irianas_client/decorators/__init__.py
@@ -12,9 +12,8 @@ def require_token(f):
         if os.path.exists(path_file_token):
             file_token = open(path_file_token)
             data_json = json.loads(file_token)
-
-            token = hashlib.sha512(request.form.data['token']).hexdigest()
-            ip = hashlib.sha512(request.form.data['ip']).hexdigest()
+            token = hashlib.sha512(request.form.get('token')).hexdigest()
+            ip = hashlib.sha512(request.form.get('ip')).hexdigest()
 
             if data_json.get('token') == token and \
                data_json.get('ip') == ip:
